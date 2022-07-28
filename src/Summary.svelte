31ea016fc30cce653d0adfa249f1f0ed697dbc2f
Martingmb <martingmb@outlook.com>
2022-07-27 23:10:17 -0500

    Fixed inline rows

--- src/Summary.svelte
+++ src/Summary.svelte
@@ -1,33 +1,30 @@
 <script>
-	import { Button, Card } from 'spaper';
-	import { slide } from 'svelte/transition';
-	import { onMount } from 'svelte';
-	import SC from './SummaryComponent.svelte'
-	import dataJson from './data';
-	
-	export let summary;
-	
-	let places = [];
-	
-	onMount(() => {
-		places.push(dataJson[summary['foodChoice']])
-		for(const activity of summary.activities) {
-			places.push(dataJson[activity])
-		}
-		places = places.sort(function(a, b){return a.startTime-b.startTime});
-	});
+  import { Button, Card } from "spaper";
+  import { slide } from "svelte/transition";
+  import { onMount } from "svelte";
+  import SC from "./SummaryComponent.svelte";
+  import dataJson from "./data";
+
+  export let summary;
+
+  let places = [];
+
+  onMount(() => {
+    places.push(dataJson[summary["foodChoice"]]);
+    for (const activity of summary.activities) {
+      places.push(dataJson[activity]);
+    }
+    places = places.sort(function (a, b) {
+      return a.startTime - b.startTime;
+    });
+  });
 </script>
 
 <div class="paper border border-secondary border-dotted">
-	<h3>
-		Itinerario, ps tomale foto 
-	</h3>
-	<div class="row flex-middle" transition:slide>
-	{#each places as place}
-		<SC bind:data={place}/>
-	{/each}
+  <h3>Itinerario, ps tomale foto</h3>
+  <div class="row" transition:slide>
+    {#each places as place}
+      <SC bind:data={place} />
+    {/each}
+  </div>
 </div>
-</div>
-
-
-
--- src/SummaryComponent.svelte
+++ src/SummaryComponent.svelte
@@ -1,22 +1,18 @@
 <script>
-	import { Card } from 'spaper';
-	import { onMount } from 'svelte';
-	export let data;
-	
-	onMount(() => {
-    
-  })
-	
-	const handleClick = async () => {
-		await navigator.clipboard.writeText(data.address);
-	}
+  import { Card } from "spaper";
+  import { onMount } from "svelte";
+  export let data;
+
+  onMount(() => {});
+
+  const handleClick = async () => {
+    await navigator.clipboard.writeText(data.address);
+  };
 </script>
 
-<div class="col-6 col">
-		<Card title={data.title} image={data.imageUrl}>
-			{data.address}
-			<p slot="bottom">
-				
-			</p>
-		</Card>
+<div class="col">
+  <Card title={data.title} image={data.imageUrl}>
+    {data.address}
+    <p slot="bottom" />
+  </Card>
 </div>
